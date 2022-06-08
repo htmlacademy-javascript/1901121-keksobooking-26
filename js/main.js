33167d67531fae645c3915961462472470e44576
Alex Sam <fesspav@mail.ru>
2022-06-08 17:25:28 +0300

    Добавил амперсанд

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Результат: целое число из диапазона "от...до"
 
 const getRandomNumber = (min, max) => {
-  if ((min >= 0) & (max > min)) {
+  if (min >= 0 && max > min) {
     const randomNumber = Math.round(Math.random() * (max - min + 1)) + min;
     return randomNumber;
   }
@@ -20,7 +20,7 @@ getRandomNumber(16, 12);
 
 // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
 const getGeoCoordinates = (min, max, amount) => {
-  if ((min >= 0) & (max > min)) {
+  if (min >= 0 && max > min) {
     const geoNumber = Math.random() * (max - min) + min;
     const geoNumberResult = geoNumber.toFixed(amount);
     return geoNumberResult;
