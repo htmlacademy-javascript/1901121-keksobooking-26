3b2e62abbc1dbabef261293615ab06a7ce62cfd9
Alex Sam <fesspav@mail.ru>
2022-06-08 16:58:54 +0300

    добавил двоеточия и верблюжий нейминг

--- js/main.js
+++ js/main.js
@@ -5,18 +5,18 @@ const sayHi = (message) => {
 }
 
 const getRandomNumber = (min, max) => {
-  if (min >= 0 & max > min) {
+  if ((min >= 0) & (max > min)) {
     const randomNumber = Math.round(Math.random() * (max - min + 1)) + min;
     return randomNumber;
-  }
+  };
   if (max < min) {
     const randomNumber = Math.round(Math.random() * (min - max)) + max;
     return randomNumber;
-  }
+  };
   if (min = max) {
     const randomNumber = min;
     return randomNumber;
-  }
+  };
 }
 
 sayHi(getRandomNumber(16, 12));
@@ -24,21 +24,21 @@ sayHi(getRandomNumber(16, 12));
 
 // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
 const getGeoCoordinates = (min, max, amount) => {
-  if (min >= 0 & max > min) {
+  if ((min >= 0) & (max > min)) {
     const geoNumber = Math.random() * (max - min) + min;
-    geoNumber_result = geoNumber.toFixed(amount);
-    return geoNumber_result;
-  }
+    geoNumberResult = geoNumber.toFixed(amount);
+    return geoNumberResult;
+  };
   if (max < min) {
     const geoNumber = Math.round(Math.random() * (min - max)) + max;
-    geoNumber_result = geoNumber.toFixed(amount);
-    return geoNumber_result;
-  }
+    geoNumberResult = geoNumber.toFixed(amount);
+    return geoNumberResult;
+  };
   if (min = max) {
     const geoNumber = min;
-    geoNumber_result = geoNumber.toFixed(amount);
-    return geoNumber_result;
-  }
+    geoNumberResult = geoNumber.toFixed(amount);
+    return geoNumberResult;
+  };
 }
 
 sayHi(getGeoCoordinates(1.2, 1.8, 5));
