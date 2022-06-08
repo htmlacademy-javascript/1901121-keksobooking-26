423370e32b996f3e092596488b3c026dc0647b69
Alex Sam <fesspav@mail.ru>
2022-06-08 16:52:37 +0300

    Исправил условия

--- js/main.js
+++ js/main.js
@@ -9,12 +9,17 @@ const getRandomNumber = (min, max) => {
     const randomNumber = Math.round(Math.random() * (max - min + 1)) + min;
     return randomNumber;
   }
-  if (max <= min) {
-    return 'Неверный диапазон';
+  if (max < min) {
+    const randomNumber = Math.round(Math.random() * (min - max)) + max;
+    return randomNumber;
+  }
+  if (min = max) {
+    const randomNumber = min;
+    return randomNumber;
   }
 }
 
-sayHi(getRandomNumber(12, 18));
+sayHi(getRandomNumber(16, 12));
 
 
 // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
@@ -24,9 +29,16 @@ const getGeoCoordinates = (min, max, amount) => {
     geoNumber_result = geoNumber.toFixed(amount);
     return geoNumber_result;
   }
-  if (max <= min) {
-    return 'Неверный диапазон';
+  if (max < min) {
+    const geoNumber = Math.round(Math.random() * (min - max)) + max;
+    geoNumber_result = geoNumber.toFixed(amount);
+    return geoNumber_result;
+  }
+  if (min = max) {
+    const geoNumber = min;
+    geoNumber_result = geoNumber.toFixed(amount);
+    return geoNumber_result;
   }
 }
 
-sayHi(getGeoCoordinates(1.2, 1.8, 6));
+sayHi(getGeoCoordinates(1.2, 1.8, 5));
