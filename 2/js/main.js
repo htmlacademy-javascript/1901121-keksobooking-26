cc593fd4c0f399782cc36a3fa0ff8d669d656d48
Keks <keks@htmlacademy.ru>
2022-06-09 12:25:14 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,30 @@
+// Результат: целое число из диапазона "от...до"
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0) {
+    return getRandomNumber(Math.abs(min), Math.abs(max));
+  }
+  if (max === min) {
+    return min;
+  }
+  if (max < min) {
+    return getRandomNumber(max, min);
+  }
+  return Math.round(Math.random() * (min - max)) + max;
+};
+
+getRandomNumber(16, 12);
+
+
+// Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
+const getGeoCoordinates = (min, max, amount) => {
+  if (min < 0 || max < 0) {
+    return getGeoCoordinates(Math.abs(min), Math.abs(max), amount);
+  }
+  if (max < min) {
+    return getGeoCoordinates(Math.abs(max), Math.abs(min), amount);
+  }
+  const geoNumber = Math.random() * (max - min) + min;
+  return parseFloat(geoNumber.toFixed(amount));
+};
+
+getGeoCoordinates(1.9, 1.8, 5);
